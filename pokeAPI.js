2ab352f7c40e1c9393e70cbf144542faff7142ae
btoombs <btoombs@Buckinghams-Mac-Pro.local>
2021-03-25 21:56:44 -0400

    initial push

---
+++ pokeAPI.js
@@ -0,0 +1,44 @@
+const pokedex = document.getElementById("pokedex");
+console.log(pokedex);
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 678; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+        promises.push(fetch(url)
+            .then((res) => res.json()));
+        }
+        Promise.all(promises).then((results) => {
+            const pokemon = results.map((data) => ({
+                name: data.name,
+                id: data.id,
+                image: data.sprites['front_default'],
+                type: data.types.map((type) => type.type.name).join(', ')
+            }));
+            displayPokemon(pokemon);
+        });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map (species => `
+    <div class="card" id="pokedex">
+        <img src="${species.image}" class="card-img" alt="${species.name}">
+        <div class="card-img-overlay">
+            <h2 class="card-title">${species.name.toUpperCase()}</h2>
+            <p class="card-text">Dex #${species.id} | Type: ${species.type}</p>
+            <p class="card-text"></p>
+        </div>
+    </div>
+
+    `)
+    pokedex.innerHTML = pokemonHTMLString;
+}
+
+fetchPokemon();
+
+// const searchBar = document.getElementById('searchBar');
+// searchBar.addEventListener('keyup', () => {
+//     console.log(e.target.value);
+// })
+
